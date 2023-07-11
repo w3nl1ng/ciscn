a20e04863268c812111aefcaf208e35445952bf3
w3nl1ng <quanjunxiong@gmail.com>
2023-07-11 11:37:57 +0800

    finish Scanner.Run and pass the test

--- scanner/run.go
+++ scanner/run.go
@@ -1,6 +1,17 @@
 package scanner
 
-// 此函数根据cmd参数调用os exec执行nmap，并返回执行的输出结果
-func (sc *Scanner) Run(cmd []string) []byte {
+import (
+	"log"
+	"os/exec"
+)
 
+// 此函数根据cmd参数调用os exec执行nmap，并返回执行的输出结果
+func (sc *Scanner) Run(args []string) []byte {
+	cmd := exec.Command("nmap", args...)
+	output, err := cmd.Output()
+	if err != nil {
+		log.Println("scanner/Run: failed to run cmd")
+		return nil
+	}
+	return output
 }
---
+++ scanner/run_test.go
@@ -0,0 +1,16 @@
+package scanner
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	args := []string{"-sL", "16.163.13.0/24"}
+	sc := NewScanner("test.txt")
+	output := sc.Run(args)
+	if output == nil {
+		t.Fatal("scanner/TestRun: sc.Run return nil")
+	}
+	fmt.Println(string(output))
+}
